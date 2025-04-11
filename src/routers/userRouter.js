d35c865f78bbe9345e0b569be338c4c8c7352e13
Neha-singh18 <singhrajputneha896@gmail.com>
2025-04-11 23:20:56 +0530

    feed api and pagination

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 const { userAuth } = require("../config/middlewares/auth");
+const { User } = require("../models/user");
 
 const USER_DATA = ["firstName", "lastName"];
 
@@ -26,8 +27,6 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
     res.status(400).send(`Error: ${err.message}`);
   }
 });
-
-
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
@@ -60,6 +59,57 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({
       message: "Coonection Found",
       data: data,
+    });
+  } catch (err) {
+    res.status(400).send(`Error: ${err.message}`);
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? (limit = 50) : limit;
+    
+    let skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        {
+          toUserId: loggedInUser._id,
+        },
+      ],
+    })
+      .select("firstName lastName status")
+      .populate("fromUserId", USER_DATA)
+      .populate("toUserId", USER_DATA);
+
+    const hiddenUsersToFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hiddenUsersToFeed.add(req.fromUserId._id.toString());
+      hiddenUsersToFeed.add(req.toUserId._id.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        {
+          _id: { $nin: Array.from(hiddenUsersToFeed) },
+        },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    })
+      .select(USER_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      data: users,
     });
   } catch (err) {
     res.status(400).send(`Error: ${err.message}`);
